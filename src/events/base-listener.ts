9a5072769584ef86162760fb054c97a9aff86faa
Brandon Shearin <brandon.shearin@ibm.com>
2021-02-11 10:43:18 -0700

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -6,7 +6,7 @@ interface Event {
   data: any;
 }
 
-abstract class Listener<T extends Event> {
+export abstract class Listener<T extends Event> {
   abstract subject: T["subject"];
   abstract queueGroupName: string;
   abstract onMessage(data: T["data"], msg: Message): void;
@@ -52,4 +52,3 @@ abstract class Listener<T extends Event> {
   }
 }
 
-export default Listener;
